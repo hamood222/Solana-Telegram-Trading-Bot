0415644dd8f508b8433fa917e20827795f1f7a1d
magicsolwiz <gladyce32stantonmpu@hotmail.com>
2025-02-19 05:51:53 +0000

    feat: remove h-trading folder

--- h-tg-trading-bot/constants/index.ts
+++
@@ -1,6 +0,0 @@
-export const commands = [
-  "/buy",
-  "/sell",
-  "/home",
-  "/wallet",
-]
--- h-tg-trading-bot/constants/msg.constants.ts
+++
@@ -1,3 +0,0 @@
-export const BUY_SUCCESS_MSG = '✅ Successfully Bought!!!'
-export const SELL_SUCCESS_MSG = '✅ Successfully Sold!!!'
-export const ORDER_SUCCESS_MSG = '✅ Order is successfully created!!!'
